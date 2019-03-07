15b17163bb7817b2b2caf2a32ea836a749c164da
Fredrik Hjärpe <fredrik.hjarpe@telia.com>
2019-03-07 23:16:16 +0100

    Arbetar

--- src/chatclient.js
+++ src/chatclient.js
@@ -1,7 +1,9 @@
 import React, { Component } from 'react';
 import './chatclient.css';
 import io from 'socket.io-client';
-let loopCounter = 0;
+import ScrollToBottom from 'react-scroll-to-bottom';
+
+//let loopCounter = 0;
 
 // Random function for the App ==================================================================================================================
 function showTipsPopUp () {
@@ -15,14 +17,18 @@ class HeaderContent extends Component {
     );
   }
 }
-// The ChatWindow ===============================================================================================================================
+// The ChatWindow - Child for Main Content ======================================================================================================
 class ChatWindow extends Component {
   constructor(props) {
     super(props);
-    this.state = { messages: [], textInput: '' };
+    this.state = { messages: [], textInput: ''};
+
     this.messegnesAdd = this.messegnesAdd.bind(this);
     this.checkStrInput = this.checkStrInput.bind(this);
-  };
+    this.setYourMess = this.setYourMess.bind(this);
+    this.messagnesSend = this.messagnesSend.bind(this);
+    this.logOut = this.logOut.bind(this);
+  }
   componentDidMount() {
     this.listen = io('http://ec2-13-53-66-202.eu-north-1.compute.amazonaws.com:3000/');
 
@@ -44,6 +50,25 @@ class ChatWindow extends Component {
   messegnesAdd(chattMessObj) {
     this.setState({ messages: [...this.state.messages, chattMessObj] });
   }
+  setYourMess(e) {
+    this.setState({ textInput: e.target.value });
+  }
+  messagnesSend() {
+    // Get both string needed for sending the mess and last reset both state and the textarea for the mess
+    let getUserName = document.querySelector('#yourUsrName').textContent;
+    let getMessStr = this.state.textInput;
+
+    let messBody = {
+        username: getUserName,
+        content: getMessStr
+      }
+
+    this.listen.emit('message', messBody);
+    this.setState({ textInput: '' });
+    document.querySelector('#chatMessegnes').value = '';
+   }
+
+
   checkStrInput(e) {
     let incommingInput = e.target.value;
     let incommingInputSplitToTypeLetter = incommingInput.split('').pop();
@@ -51,22 +76,20 @@ class ChatWindow extends Component {
     if (incommingInputSplitToTypeLetter === 'ö') {
       console.log('Fel tecken');
     }
-    // let strArray = [];
-    // strArray.push();
-    // console.log(strArray.split(''));
-    // let getLastArrStr = strArray.pop();
-    // console.log(getLastArrStr);
-    // this.setState({textInput: e.target.value})
+  }
 
+  logOut() {
 
   }
+
   render() {
-    let textAreaFocus = this.state.textInput;
     return (
       <section>
+        <p id="chatWindow">Chatmeddelanden</p> <button id="closeBtn" onClick={ this.logOut } >Logga Ut</button>
+        <p id="usrStrView2">Ditt användarnamn: <span id="yourUsrName">{ this.props.sendUsrName }</span></p>
         <fieldset>
           <legend>Meddelanden</legend>
-            <section id="messagnesReceive">
+            <ScrollToBottom className="messagnesReceive">
               {
                 this.state.messages.map(obj => {
                   //console.log(this.state.messages);
@@ -78,45 +101,61 @@ class ChatWindow extends Component {
                       <div className="messContent" >
                       { obj.content }
                       </div>
-                      <hr className="messMiddleLine"/>
+                      <hr className="middleLine"/>
                     </section>
                   );
                 })
               }
-            </section>
+            </ScrollToBottom>
+            <hr className="middleLine"/>
         </fieldset>
         <fieldset id="messagneSend">
           <legend>Ditt meddelande <span className="inputReq"> *</span> </legend>
-          <textarea id="chatMessegnes" maxLength="200" placeholder="Max 200 tecken" onChange={ this.checkStrInput } required></textarea>
+            <textarea id="chatMessegnes" maxLength="200" onChange={ this.setYourMess } required></textarea>
+            <div id="finishMess"> <p> / 200 </p> <button id="sendBtn" onClick={ this.messagnesSend }> Sänd</button></div>
         </fieldset>
       </section>
     );
   }
 }
-// The ChatWindow ===============================================================================================================================
+// Main Content =================================================================================================================================
 let listen;
 class MainContent extends Component {
   constructor(props) {
     super(props);
+    // Is sending into the ChatWindow
     this.state = { loginUsrName: '' };
+    // ==============================
+
+    this.logIn = this.logIn.bind(this);
     this.setYourUserName = this.setYourUserName.bind(this);
-    //this.setYourMess = this.setYourMess.bind(this);
   }
   setYourUserName(e) {
     let incommingInput = e.target.value;
-    let getAlphanumeric = incommingInput.replace(/[^a-z0-9]/gi, '');
+    this.setState({ loginUsrName: incommingInput });
 
-    // Ta upp, detta fungerar men inte att lägga det i samma if med ||?
-    if (getAlphanumeric) {
-      this.setState({ loginUsrName: getAlphanumeric });
-    }
-    else if (incommingInput === '-'  || incommingInput === '_') {
-      this.setState({ loginUsrName: getAlphanumeric });
-    }
-    // Check for just alphanumeric characters, -, _ and spaces
+    // if (incommingInput./[a-z0-9]/) {
+    //   console.log('few');
+    // }
+    // let getAlphanumeric = /incommingInput/.test(/[a-z0-9]i/);
+    // console.log(getAlphanumeric);
 
 
-    // Validate the inputed usernamne
+    // Validate the inputed usernamne in real time
+
+
+    //
+    //
+    // let incommingInputSplitToTypeLetter = incommingInput.split('').pop();
+    // console.log(getAlphanumeric);
+    //
+    // if (getAlphanumeric) {
+    // }
+    // // Check for just alphanumeric characters, -, _ and spaces
+    // else if (incommingInput === '-'  || incommingInput === '_') {
+    // }
+    // // Send a error if fault
+    //
 
     // let incommingInputSplitToTypeLetter = incommingInput.split('').pop();
     // console.log(incommingInputSplitToTypeLetter);
@@ -129,35 +168,25 @@ class MainContent extends Component {
     // let getLastArrStr = strArray.pop();
     // console.log(getLastArrStr);
     // this.setState({textInput: e.target.value})
-
-
+  }
+  logIn() {
 
   }
-
-  // setYourMess(e) {
-  //   this.setState({ loginUsrName: e.target.value });
-  // }
-
   render() {
-    let inputUsrName = this.state.loginUsrName;
     return (
+      //
       <div>
         <section id="view1">
           <div id="labelContainer">
             <label htmlFor="usernameStr" id="usrInputStr">Användarnamn <span className="inputReq"> *</span> </label></div>
             <div id="userContainer">
               <input type="text" id="usernameStr" minLength="1" maxLength="12" onChange={ this.setYourUserName } defaultValue={ this.state.loginUsrName } required/>
-              <button id="usernameBtn">Logga In!</button>
+              <button id="usernameBtn" onClick={ this.logIn } >Logga In!</button>
             </div>
             <p id="showTipsPopUp">Vanliga tecken & siffor (- _  mellanslag. Max 12 tecken!)</p>
         </section>
         <section id="view2">
-          <p id="chatWindow">Chatmeddelanden</p>
-          <p id="usrStrView2">Ditt användarnamn: <span id="yourUsrName">{ inputUsrName }</span></p>
-          <section id="chatContainer">
-            <ChatWindow/>
-          </section>
-          <button id="closeBtn">Logga Ut</button>
+          <ChatWindow sendUsrName={ this.state.loginUsrName }/>
         </section>
       </div>
     );
